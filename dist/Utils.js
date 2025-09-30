ec5ea4c31213e0a19d559b88fa1c3fff826a155f
Momoe <momoemothana@gmail.com>
2025-09-30 15:59:11 +0700

    Update js

---
+++ dist/Utils.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function helloWorld() {
+    console.log("Hello, World!");
+}
+function add(a, b) {
+    return a + b;
+}
+const Utils = {
+    helloWorld,
+    add
+};
+module.exports = Utils;
+//# sourceMappingURL=Utils.js.map
--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ const Express = require("express");
 const app = Express();
 const port = 3000;
 app.get("/", (req, res) => {
-    res.send("Hellosadsadoasdoasodkas");
+    res.send("Hello, World!");
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
